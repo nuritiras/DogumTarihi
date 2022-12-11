5ea9651d9cc34c9a7ed5b8d18d9bd93919243a4e
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-12-11 23:07:59 +0300

    Proje dosyası ekle.

---
+++ DogumTarihi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogumTarihi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            YasSinifi yasSinifi = new YasSinifi();
+
+            DateTime Tarih = DateTime.Now;
+            Console.WriteLine("Yaş="+ yasSinifi.Yas(1976));
+
+            var date1 = new DateTime(1976, 01, 01);
+            Console.Write("Gün Farkı="+ yasSinifi.Yas(date1).Days);
+
+            Console.ReadLine();
+        }
+    }
+    class YasSinifi
+    {
+        public int Yas(int dogum_yili)
+        {
+            DateTime simdi = DateTime.Now;
+            return simdi.Year - dogum_yili;
+        }
+
+        public TimeSpan Yas(DateTime dogum_yili)
+        {
+            DateTime simdi = DateTime.Now;
+            TimeSpan GunFarki = simdi-dogum_yili;
+            return GunFarki;
+        }
+    }
+}
---
+++ DogumTarihi/YasSinifiBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DogumTarihi
+{
+    internal class YasSinifiBase
+    {
+        public int Yas(int dogum_yili)
+        {
+            DateTime yil;
+            yil = DateTime.Now;
+            return yil.Date.Year - dogum_yili; ;
+        }
+    }
+}
